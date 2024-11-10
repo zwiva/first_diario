b3670394aa66916300655df569f0e5af4af54f7a
Andrea Castro <ap.castrocastellon@gmail.com>
2024-11-10 04:39:25 -0300

    estructura base: mejora footer

--- js/components/shared/footer.js
+++ js/components/shared/footer.js
@@ -1,11 +1,22 @@
 export function footerComponent() {
   return `
     <div class="footer">
+      
       <div class="footer-body">
-        <a class="footer-item" href="index.html">Home</a>
-        <a class="footer-item" href="articles.html">Registrarse</a>
-        <a class="footer-item" href="login.html">Iniciar sesión</a>
+      
+        <div style="display: flex; text-align: center;">
+          <img src="" style="background-color: white;height: 80px; width: 80px; margin:0.5em;">
+          <h3>El faro de la IA</h3>
+        </div>
+      
+        <div style="margin: 1em;">
+          <a class="footer-item" href="index.html">Home</a>
+          <a class="footer-item" href="registro.html">Registrarse</a>
+          <a class="footer-item" href="login.html">Iniciar sesión</a>
+        </div>
+    
       </div>
+    
     </div>
   `;
 }
